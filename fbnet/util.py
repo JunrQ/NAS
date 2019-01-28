64c616e6200d3d86904472f6807d77d16adc9804
anorthman <940769395@qq.com>
2019-01-28 03:16:03 -0500

    update CosineDecayScheduler_Grad

--- fbnet/util.py
+++ fbnet/util.py
@@ -77,38 +77,51 @@ def get_train_ds(args, kv=None):
         rank, nworker = kv.rank, kv.num_workers
     train = mx.io.ImageRecordIter(
         path_imgrec         = args.train_rec_path,
-        label_width         = 1,
-        mean_r              = 123.0,
-        mean_g              = 116.0,
-        mean_b              = 103.0,
-        scale               = 0.01,
-        data_name           = 'data',
-        label_name          = 'softmax_label',
-        data_shape          = tuple(map(int, args.image_shape.split(','))),
-        batch_size          = args.batch_size,
-        resize_height       = 400,
-        resize_width        = 400,
-        patch_size          = args.patch_size,
-        patch_idx           = args.patch_idx,
-        do_aug              = True,
-        aug_seq             = 'aug_face',
-        FacePatchSize_Main  = 267,
-        FacePatchSize_Other = 128,
-        PatchFullSize        = 128 if args.image_shape.split(',')[-1] == '108' else 256,
-        PatchCropSize        = 108 if args.image_shape.split(',')[-1] == '108' else 224,
-        illum_trans_prob    = args.illum_trans_prob,
-        gauss_blur_prob     = 0.3,
-        motion_blur_prob    = 0.1,
-        jpeg_comp_prob    = 0.4,
-        res_change_prob    = 0.4,
-        hsv_adjust_prob    = args.hsv_adjust_prob,
-        preprocess_threads  = args.data_nthreads,
-        shuffle             = True,
-        num_parts           = nworker,
-        part_index          = rank,
-        force2gray = args.force2gray,
-        force2color = args.force2color,
-        isgray='true' if args.isgray else 'false')
+        data_shape          =(3, 108, 108),
+        batch_size          =args.batch_size,
+        resize              =128,
+        shuffle             =True,
+        prefetch_buffer     =4, ##default
+        max_rotato_angle    =90,
+        random_resized_crop =True,
+        max_random_area     =65536,
+        min_random_area     =11664,
+
+        )
+ #   train = mx.io.ImageRecordIter(
+ #       path_imgrec         = args.train_rec_path,
+ #       label_width         = 1,
+ #       mean_r              = 123.0,
+ #       mean_g              = 116.0,
+ #       mean_b              = 103.0,
+ #       scale               = 0.01,
+ #       data_name           = 'data',
+ #       label_name          = 'softmax_label',
+ #       data_shape          = tuple(map(int, args.image_shape.split(','))),
+ #       batch_size          = args.batch_size,
+ #       resize_height       = 400,
+ #       resize_width        = 400,
+ #       patch_size          = args.patch_size,
+ #       patch_idx           = args.patch_idx,
+ #       do_aug              = True,
+ #       #aug_seq             = 'aug_face',
+ #       #FacePatchSize_Main  = 267,
+ #       #FacePatchSize_Other = 128,
+ #       PatchFullSize        = 128 if args.image_shape.split(',')[-1] == '108' else 256,
+ #       PatchCropSize        = 108 if args.image_shape.split(',')[-1] == '108' else 224,
+ #       illum_trans_prob    = args.illum_trans_prob,
+ #       gauss_blur_prob     = 0.3,
+ #       motion_blur_prob    = 0.1,
+ #       jpeg_comp_prob    = 0.4,
+ #       res_change_prob    = 0.4,
+ #       hsv_adjust_prob    = args.hsv_adjust_prob,
+ #       preprocess_threads  = args.data_nthreads,
+ #       shuffle             = True,
+ #       num_parts           = nworker,
+ #       part_index          = rank,
+ #       force2gray = args.force2gray,
+ #       force2color = args.force2color,
+ #       isgray='true' if args.isgray else 'false')
     return train
 
 def inv_gumbel_cdf(y, mu=0.0, beta=1.0, eps=1e-20):
@@ -182,6 +195,8 @@ def get_mnist_iter(args):
 
 def ce(label, pred):
     label = label.ravel()
+    print "label.shape",label.shape
+    print "pred.shape",pred.shape
     assert label.shape[0] == pred.shape[0]
     prob = pred[np.arange(label.shape[0]), np.int64(label)]
     return -np.log(prob + 1e-9).sum()
@@ -217,25 +232,36 @@ class CosineDecayScheduler_Grad(LRScheduler):
 
     def __call__(self, num_update):
         T_cur = num_update - self.last_restart_step
-        assert T_cur >= 0
-        if T_cur >= self.T_0:
+        print "num_date",num_update
+        assert T_cur > 0
+        if T_cur > self.T_0:
             # restart
             self.last_restart_step = num_update
             self.init_lr *= self.m_mul
             self.base_lr = self.init_lr
-            self.mini_lr = self.init_lr * self.alpha
+            self.mini_lr = self.init_lr * self.alpha / self.m_mul
             self.rise_grad = (self.init_lr - self.mini_lr) / self.rise_region
             _logger.info("Update[%d]: Change learning rate to %f rise_grad is %f",num_update,self.base_lr,self.rise_grad)
 
-            #self.T_0 = int(self.T_0 * self.t_mul)
+            self.T_0 = int(self.T_0 * self.t_mul)
             self.first_flag = False
+   
+        if T_cur <= self.rise_region and self.first_flag == False:
+            self.base_lr = self.mini_lr+self.rise_grad * T_cur
+            if T_cur == self.rise_region:
+                self.last_restart_step = num_update
+                self.first_flag == True
         else:
-            if T_cur <= self.rise_region and self.first_flag == False:
-                self.base_lr = self.mini_lr+self.rise_grad * T_cur
-            else:
-                cosine_decay = 0.5 * (1 + math.cos(math.pi * T_cur / (self.T_0 - self.rise_region) ))
-                decayed = (1 - self.alpha) * cosine_decay + self.alpha
-                self.base_lr = self.init_lr * decayed
+            cosine_decay = 0.5 * (1 + math.cos(math.pi * T_cur / self.T_0 ))
+            decayed = (1 - self.alpha) * cosine_decay + self.alpha
+            self.base_lr = self.init_lr * decayed
+       # else:
+       #     if T_cur <= self.rise_region and self.first_flag == False:
+       #         self.base_lr = self.mini_lr+self.rise_grad * T_cur
+       #     else:
+       #         cosine_decay = 0.5 * (1 + math.cos(math.pi * T_cur / (self.T_0 - self.rise_region) ))
+       #         decayed = (1 - self.alpha) * cosine_decay + self.alpha
+       #         self.base_lr = self.init_lr * decayed
 
         return self.base_lr
 
